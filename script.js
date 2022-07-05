2ac6280e3b6733e102f8252beef5d58262fa2d97
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-07-05 02:09:50 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let haveSpouse = document.querySelector('#haveSpouse')
+let dataSpouse = document.querySelector('.dataSpouse')
+
+let haveDependents = document.querySelector('#haveDependents')
+let dataDependents = document.querySelector('.dataDependents')
+
+let haveFed = document.querySelector('#haveFed')
+let dataFed = document.querySelector('.dataFed')
+
+let haveMedicalExpenses = document.querySelector('#haveMedicalExpenses')
+let dataMedicalExpenses = document.querySelector('.dataMedicalExpenses')
+
+let haveEducationExpenses = document.querySelector('#haveEducationExpenses')
+let dataEducationExpenses = document.querySelector('.dataEducationExpenses')
+
+const setup = () => {
+    haveSpouse.addEventListener('change', onChange)
+    haveDependents.addEventListener('change', onChange)
+    haveFed.addEventListener('change', onChange)
+    haveMedicalExpenses.addEventListener('change', onChange)
+    haveEducationExpenses.addEventListener('change', onChange)
+}
+
+const onChange = (event) => {
+    const target = event.target.value
+    const targetId = event.target.id
+
+    if (target == "sim") {
+
+        switch (targetId) {
+            case "haveSpouse":
+                dataSpouse.classList.toggle('active')
+                break;
+
+            case "haveDependents":
+                dataDependents.classList.toggle('active')
+                break;
+
+            case "haveFed":
+                dataFed.classList.toggle('active')
+                break;
+
+            case "haveMedicalExpenses":
+                dataMedicalExpenses.classList.toggle('active')
+                break;
+
+            case "haveEducationExpenses":
+                dataEducationExpenses.classList.toggle('active')
+                break;
+
+            default:
+                break;
+        }
+
+    } else if (target == "não") {
+
+        switch (targetId) {
+            case "haveSpouse":
+                dataSpouse.classList.remove('active')
+                break;
+
+            case "haveDependents":
+                dataDependents.classList.remove('active')
+                break;
+
+            case "haveFed":
+                dataFed.classList.remove('active')
+                break;
+
+            case "haveMedicalExpenses":
+                dataMedicalExpenses.classList.remove('active')
+                break;
+
+            case "haveEducationExpenses":
+                dataEducationExpenses.classList.remove('active')
+                break;
+
+
+            default:
+                break;
+        }
+    }
+}
+
+window.addEventListener('load', setup)
